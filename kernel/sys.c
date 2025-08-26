0863651b08ea6325bfd9a70b9ab765293342ae23
Thomas Turner <thomas_turner@talktalk.net>
2025-08-26 22:02:30 +0300

    syscall: also spoof uprobestats
    Change-Id: I61c8db639913d4d7d83da13dbb7593e01f0cb836
    Signed-off-by: ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>

--- kernel/sys.c
+++ kernel/sys.c
@@ -1262,7 +1262,8 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 	memcpy(&tmp, utsname(), sizeof(tmp));
 	if (!strncmp(current->comm, "bpfloader", 9) ||
 	    !strncmp(current->comm, "netbpfload", 10) ||
-	    !strncmp(current->comm, "netd", 4)) {
+	    !strncmp(current->comm, "netd", 4) ||
+	    !strncmp(current->comm, "uprobestats", 11)) {
 		if (current_uid().val == 0) {
 			strcpy(tmp.release, "5.4.186");
 			pr_debug("fake uname: %s/%d release=%s\n",
